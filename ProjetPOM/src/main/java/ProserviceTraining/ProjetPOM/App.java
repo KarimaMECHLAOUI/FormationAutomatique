a3e80c083a05b70368bc71ab522c840ed3403e48
karima.mechlaoui <karima.mechlaoui@LAP-TUN-31GKWL3>
2024-01-23 22:11:19 +0100

    Version 1

---
+++ ProjetPOM/src/main/java/ProserviceTraining/ProjetPOM/App.java
@@ -0,0 +1,13 @@
+package ProserviceTraining.ProjetPOM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ProjetPOM/src/test/java/Run/Runner.java
@@ -0,0 +1,25 @@
+package Run;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		
+		tags= "",
+		features= "",
+		glue="",
+		publish = true
+	
+		
+		)
+  
+
+
+
+public class Runner {
+
+}
